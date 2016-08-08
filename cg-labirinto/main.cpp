bcbef6306320a0e5c4c9c61c59d86d810eafa66b
Gustavo Tinoco <gustavotinocoo@gmail.com>
2016-08-08 17:36:54 -0400

    ajuste

--- cg-labirinto/main.cpp
+++ cg-labirinto/main.cpp
@@ -15,10 +15,36 @@ int pegaLado();
 int verificaEntradaSaida(int entrada, int saida, int linha, int coluna);
 int lado;
 void display();
-void desenhaLabinrinto();
-void keyboard(int key, int x, int y);
+void desenhaLabirinto();
 int linha,coluna,entradaLinha,entradaColuna,saidaLinha,saidaColuna;
 int labirinto[100][100];
+void janela(void)
+{
+
+    printf("\nevento de desenho...\n");
+}
+
+void teclado(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'q':
+        exit(0);
+        break;
+    case 'r':
+        glutDisplayFunc(desenhaLabirinto);
+        printf("REINICIA");
+        break;
+    case 'a':
+        glutDisplayFunc(travessiaLabirinto);
+        glutPostRedisplay();
+        printf("\nGera caminho do desenho");
+        break;
+    default:
+        printf("\nevento de teclado\n");
+        break;
+    }
+}
 
 int main(int argc, char *argv)
 {
@@ -28,7 +54,6 @@ int main(int argc, char *argv)
     scanf("%d",&linha);
     printf("\nImpressao do labirinto preenchido...\n\n\n");
 
-   // labirinto[linha][coluna];
     for (int i = 0; i < linha; i++)
     {
         for (int j = 0; j < coluna; j++)
@@ -39,7 +64,7 @@ int main(int argc, char *argv)
         printf("\n");
     }
 
-//    int entradaLinha, entradaColuna, saidaLinha, saidaColuna;
+
     printf("\n\n Digite a linha entrada de 0 ate' %d: ", linha -1);
     scanf("%d",&entradaLinha);
     printf("\n\n Digite a coluna entrada de 0 ate' %d: ", coluna -1);
@@ -64,31 +89,26 @@ int main(int argc, char *argv)
     }
 
 
-  glutInit(&argc, &argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowSize(400, 400);
-	//glutInitWindowPosition(150, 150);
-	glutCreateWindow("Labirinto");
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-1, coluna + 1, linha + 1, -1);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-
-	glutDisplayFunc(display);
-
-//	glutMouseFunc(mouse);
-	glutMainLoop();
-    //glutSpecialFunc(teclado);
-
+    glutInit(&argc, &argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(400, 400);
+    //glutInitWindowPosition(150, 150);
+    glutCreateWindow("Labirinto");
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1, coluna + 1, linha + 1, -1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(teclado);
+    glutMainLoop();
 
     /*
 
     int entrada[entradaLinha][entradaColuna];
     int d=entradaLinha,e=entradaColuna;
 
-int fr = 0;
+    int fr = 0;
     while(entrada[d][e] != labirinto[saidaLinha][saidaColuna])
     {
 
@@ -152,61 +172,95 @@ int verificaEntradaSaida(int entrada, int saida, int linha, int coluna)
 }
 
 
-void display(){
+void display()
+{
     glColor3d(0, 0, 0);
     glClearColor(255, 255, 255, 0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    desenhaLabinrinto();
-    travessiaLabirinto();
+    desenhaLabirinto();
+    //glFlush();
+}
+
+void desenhaLabirinto()
+{
+    for (int i = 0; i<linha; i++)
+        for (int j = 0; j<coluna; j++)
+        {
+            glBegin(GL_QUADS);
+            if (labirinto[i][j] == 0)
+                glColor3f(1.0, 1.0, 1.0);
+            else
+                glColor3f(0.3, 0.3, 0.3);
+
+            if(labirinto[i][j] == labirinto[saidaLinha][saidaColuna])
+                glColor3f(1.0, 0.0, 0.0);
+
+            if(labirinto[i][j] == labirinto[entradaLinha][entradaColuna])
+                glColor3f(0.3, 1.0, 0.0);
+
+            glVertex2i(j, i);
+            glVertex2i(j, i + 1);
+            glVertex2i(j + 1, i + 1);
+            glVertex2i(j + 1, i);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            glColor3f(0.6, 0.6, 0.6);
+            glVertex2i(j, i);
+            glVertex2i(j, i + 1);
+            glVertex2i(j + 1, i + 1);
+            glVertex2i(j + 1, i);
+            glEnd();
+        }
     glFlush();
-
 }
 
-void desenhaLabinrinto(){
-	for (int i = 0; i<linha; i++)
-		for (int j = 0; j<coluna; j++)
-		{
-		glBegin(GL_QUADS);
-		if (labirinto[i][j] == 0)
-			glColor3f(1.0, 1.0, 1.0);
-		else
-			glColor3f(0.3, 0.3, 0.3);
 
-        if(labirinto[i][j] == labirinto[saidaLinha][saidaColuna])
-            glColor3f(1.0, 0.0, 0.0);
 
-		if(labirinto[i][j] == labirinto[entradaLinha][entradaColuna])
-            glColor3f(0.3, 1.0, 0.0);
 
-		glVertex2i(j, i);
-		glVertex2i(j, i + 1);
-		glVertex2i(j + 1, i + 1);
-		glVertex2i(j + 1, i);
-		glEnd();
-		glBegin(GL_LINE_LOOP);
-		glColor3f(0.6, 0.6, 0.6);
-		glVertex2i(j, i);
-		glVertex2i(j, i + 1);
-		glVertex2i(j + 1, i + 1);
-		glVertex2i(j + 1, i);
-		glEnd();
-		}
-}
-
-/*void keyboard(int key, int x, int y) {
-}*/
 void travessiaLabirinto()
 {
+    printf("entrou");
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();//inicializa matriz com a matriz identidade
+    glClear(GL_COLOR_BUFFER_BIT);//Limpa janela
 
+   for (int i = 0; i<linha; i++)
+        for (int j = 0; j<coluna; j++)
+        {
+            glBegin(GL_QUADS);
+            if (labirinto[i][j] == 0)
+                glColor3f(1.0, 1.0, 1.0);
+            else
+                glColor3f(0.3, 0.3, 0.3);
+
+            if(labirinto[i][j] == labirinto[saidaLinha][saidaColuna])
+                glColor3f(1.0, 0.0, 0.0);
+
+            if(labirinto[i][j] == labirinto[entradaLinha][entradaColuna])
+                glColor3f(0.3, 1.0, 0.0);
+
+            glVertex2i(j, i);
+            glVertex2i(j, i + 1);
+            glVertex2i(j + 1, i + 1);
+            glVertex2i(j + 1, i);
+            glEnd();
+            glBegin(GL_LINE_LOOP);
+            glColor3f(0.6, 0.6, 0.6);
+            glVertex2i(j, i);
+            glVertex2i(j, i + 1);
+            glVertex2i(j + 1, i + 1);
+            glVertex2i(j + 1, i);
+            glEnd();
+        }
+    glFlush();
 }
 
 int pegaLado()
 {
-  if(lado == 3)
-    lado = 0;
+    if(lado == 3)
+        lado = 0;
     else lado++;
-  return lado;
-    //return rand() % 4;
+    return lado;
     //return rand() % 4;
 
 }
